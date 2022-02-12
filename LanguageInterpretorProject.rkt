be60aa4a9fce13e9cc6c1b73edd1c5ab06c28b26
Alex Vaslow <abv10@case.edu>
2022-02-11 20:56:37 -0500

    Mvalue function started

--- LanguageInterpretorProject.rkt
+++ LanguageInterpretorProject.rkt
@@ -1,1 +1,37 @@
 #lang racket
+
+(define mvalue
+  (lambda (lis)
+    (cond
+      [(number? lis)lis]
+      [(boolean? lis) lis]
+      [(eq? (operator lis) '*)(* (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(eq? (operator lis) '+)(+ (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(eq? (operator lis) '-)(- (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      ;still have to figure out the unary - operator (maybe check if there are two elements in iist or one
+      [(eq? (operator lis) '/)(quotient (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(eq? (operator lis) '%)(modulo (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(and (eq? (operator lis) '==) (boolean? (mvalue (firstexpression lis))))(eq? (mvalue (firstexpression lis))(mvalue (secondexpression lis)))]
+      [(and (eq? (operator lis) '!=) (boolean? (mvalue (firstexpression lis))))(not (eq? (mvalue (firstexpression lis))(mvalue (secondexpression lis))))];might have to check more here
+      [(and (eq? (operator lis) '==) (number? (mvalue (firstexpression lis))))(= (mvalue (firstexpression lis))(mvalue (secondexpression lis)))]
+      [(and (eq? (operator lis) '!=) (number? (mvalue (firstexpression lis))))(not (= (mvalue (firstexpression lis))(mvalue (secondexpression lis))))]
+      [(eq? (operator lis) '<)(< (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(eq? (operator lis) '>)(> (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(eq? (operator lis) '>=)(>= (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(eq? (operator lis) '<=)(<= (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(and (eq? (operator lis) '!) (boolean? (mvalue (firstexpression lis))))(not (mvalue (firstexpression lis)))]
+      [(and (eq? (operator lis) '||) (boolean? (mvalue (firstexpression lis))))(or (mvalue (firstexpression lis)) (mvalue (secondexpression lis)))]
+      [(and (eq? (operator lis) '&&) (boolean? (mvalue (firstexpression lis))))(and (mvalue (firstexpression lis))(mvalue (secondexpression lis)))]
+      )))
+
+(define operator
+  (lambda (lis)
+    (car lis)))
+
+(define firstexpression
+  (lambda (lis)
+    (cadr lis)))
+
+(define secondexpression
+  (lambda (lis)
+    (caddr lis)))
